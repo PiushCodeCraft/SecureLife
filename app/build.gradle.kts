8204cd2df5b0ef696d8868fc97d22da15a0ff698
piush saha <piushsaha162@gmail.com>
2026-03-12 17:10:15 +0530

    File created

---
+++ app/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    id("com.android.application")
+    id("com.google.gms.google-services")
+}
+
+android {
+
+    namespace = "com.example.womensafteyapp"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.womensafteyapp"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+}
+
+dependencies {
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    implementation("com.google.android.material:material:1.11.0")
+
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+
+    implementation("com.google.firebase:firebase-auth:22.3.0")
+
+    // Google Login
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
+
+    // Phone OTP Login
+    implementation("com.google.android.gms:play-services-auth-api-phone:18.0.1")
+}
